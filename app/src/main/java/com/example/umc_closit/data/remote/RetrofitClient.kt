02f364d27f33e50a832e4f9f2dd66bbf0fbddf13
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2025-02-17 03:22:58 +0900

    [0217] Feat: 헤더 일괄 적용

--- app/src/main/java/com/example/umc_closit/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/umc_closit/data/remote/RetrofitClient.kt
@@ -1,30 +1,47 @@
 package com.example.umc_closit.data.remote
 
-import com.example.umc_closit.data.remote.battle.BattleApiService
+import android.content.Context
 import com.example.umc_closit.data.remote.auth.AuthService
+import com.example.umc_closit.data.remote.battle.BattleApiService
 import com.example.umc_closit.data.remote.profile.ProfileService
 import com.example.umc_closit.data.remote.timeline.TimelineService
+import com.example.umc_closit.utils.TokenUtils
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "http://54.180.132.28:8080/"
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    private lateinit var retrofit: Retrofit
 
-    val instance: BattleApiService by lazy {
-        Retrofit.Builder()
+    fun init(context: Context) {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(context))
+            .build()
+
+        retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(BattleApiService::class.java)
     }
 
-    val authService: AuthService = retrofit.create(AuthService::class.java)
-    val timelineService: TimelineService = retrofit.create(TimelineService::class.java)
-    val profileService: ProfileService = retrofit.create(ProfileService::class.java)
+    val authService: AuthService by lazy {
+        retrofit.create(AuthService::class.java)
+    }
 
+    val timelineService: TimelineService by lazy {
+        retrofit.create(TimelineService::class.java)
+    }
+
+    val profileService: ProfileService by lazy {
+        retrofit.create(ProfileService::class.java)
+    }
+
+    val battleService: BattleApiService by lazy {
+        retrofit.create(BattleApiService::class.java)
+    }
 }
