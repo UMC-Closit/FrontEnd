2c3f7927a4e1bfcbccbfb5b99dff8b81a93af8de
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2025-08-02 19:48:43 +0900

    feat: 배틀 게시판 ui 개선

--- app/src/main/java/com/example/umc_closit/ui/community/battle/NewBattleDetailActivity.kt
+++ app/src/main/java/com/example/umc_closit/ui/community/battle/NewBattleDetailActivity.kt
@@ -11,14 +11,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.umc_closit.R
-import com.example.umc_closit.data.BattlePostRequest
-import com.example.umc_closit.data.BattlePostResponse
+import com.example.umc_closit.data.remote.battle.BattlePostRequest
 import com.example.umc_closit.data.remote.RetrofitClient
 import com.example.umc_closit.ui.timeline.TimelineActivity
 import com.example.umc_closit.utils.TokenUtils
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class NewBattleDetailActivity : AppCompatActivity() {
 
